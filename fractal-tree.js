98dc3785b6ed17aaab20367191497bd5b301d17e
Hans Kirchner <hans-kirchner@gmx.de>
2026-02-21 18:10:19 +0100

    Add leaves for the tree

--- fractal-tree.js
+++ fractal-tree.js
@@ -1,16 +1,18 @@
+const namespace = "http://www.w3.org/2000/svg";
+
 function drawLine(svg, x1, y1, x2, y2, depth) {
-  const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+  const line = document.createElementNS(namespace, "line");
   line.setAttribute("x1", x1);
   line.setAttribute("y1", y1);
   line.setAttribute("x2", x2);
   line.setAttribute("y2", y2);
-  line.setAttribute("stroke", "#4e8c4e");
+  line.setAttribute("stroke", "#6B492B");
   line.setAttribute("stroke-width", Math.max(1, depth) / 2);
   svg.appendChild(line);
 }
 
 // Fractal Tree Renderer
-function drawFractalTree(svg, x1, y1, angle, depth, branchLength) {
+function drawFractalTree(svg, x1, y1, angle, depth, branchLength, leaves) {
   if (depth <= 0) return;
   const rad = (angle * Math.PI) / 180;
   const x2 = x1 + branchLength * Math.sin(rad);
@@ -18,15 +20,35 @@ function drawFractalTree(svg, x1, y1, angle, depth, branchLength) {
 
   drawLine(svg, x1, y1, x2, y2, depth);
 
-  drawFractalTree(svg, x2, y2, angle - 20, depth - 1, branchLength * 0.7);
-  drawFractalTree(svg, x2, y2, angle + 45, depth - 1, branchLength * 0.7);
+  if (depth === 1) {
+    leaves.push({ x: x2, y: y2 });
+  }
+
+  drawFractalTree(
+    svg,
+    x2,
+    y2,
+    angle - 20,
+    depth - 1,
+    branchLength * 0.7,
+    leaves,
+  );
+  drawFractalTree(
+    svg,
+    x2,
+    y2,
+    angle + 45,
+    depth - 1,
+    branchLength * 0.7,
+    leaves,
+  );
 }
 
 function renderFractalTree() {
   const container = document.querySelector(".fractal-tree");
   if (!container) return;
   container.innerHTML = "";
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const svg = document.createElementNS(namespace, "svg");
   svg.setAttribute("width", 350);
   svg.setAttribute("height", 350);
 
@@ -38,6 +60,8 @@ function renderFractalTree() {
   const depth = 12;
   const trunkLenth = 50;
 
+  const leaves = [];
+
   // Draw the fractal tree
   drawFractalTree(
     svg,
@@ -46,6 +70,7 @@ function renderFractalTree() {
     0,
     depth,
     100,
+    leaves,
   );
 
   // Draw the trunk
@@ -57,6 +82,18 @@ function renderFractalTree() {
     viewBoxHeight,
     depth,
   );
+
+  for (const coord of leaves) {
+    // Draw a small leaf at the end of the branch
+    const leaf = document.createElementNS(namespace, "circle");
+    leaf.setAttribute("cx", coord.x);
+    leaf.setAttribute("cy", coord.y);
+    leaf.setAttribute("r", 1.5);
+    leaf.setAttribute("fill", "#ec9f9f");
+    svg.appendChild(leaf);
+  }
+
+  console.log(`Number of leaves: ${leaves.length}`);
 
   container.appendChild(svg);
 }
